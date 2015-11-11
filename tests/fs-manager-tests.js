eac18a8bd308c20ad8758f0a4f538ca5498ff1cf
Tony McBeth <tony.mcbeth@gmail.com>
2015-11-11 19:13:23 +0000

    Updated tests

--- tests/fs-manager-tests.js
+++ tests/fs-manager-tests.js
@@ -1,15 +1,53 @@
 require('chai').should();
-var generator = require('../src/generator');
-
-describe("When getting repo name from project name", function(){
-	it('Then the repo name should be the project name with hash rather than spaces and lowercase', function(){
-		var expectedRepoName = "code-creator";
-		var givenProjectName = "Code Creator";
-		var stringParser = new StringParser();
-		var repoName = stringParser.hyphenateAndLowerCase(givenProjectName);
-
-		repoName.should.equal(expectedRepoName);
 
 
+var createdDirectories = [];
+
+var fakeFs = {
+	mkdirSync: function(name) { 
+		createdDirectories.push(name);
+	},
+	readFileSync: function(file, encoding) {
+		return "Test Data"
+	},
+	writeFile: function(path, data, encoding, callback) {
+		callback();
+	}
+}
+
+describe("When creating a directory structure", function(){
+	it('Then the created structure should match the desired structure', function(){
+		
+		var subjectUnderTest = require('../src/fs-manager')(fakeFs, '');
+
+		var rootDirectory = "Folder0";
+
+		var directoryStructure = {
+			"Folder1": {
+				"Folder2": {},
+				"Folder3": {}
+			}
+		};
+
+		subjectUnderTest.generateDirectoryStructure(directoryStructure, rootDirectory);
+		createdDirectories.length.should.eq(4);
+		createdDirectories[0].should.eq('Folder0');
+		createdDirectories[1].should.eq('Folder0/Folder1');
+		createdDirectories[2].should.eq('Folder0/Folder1/Folder2');
+		createdDirectories[3].should.eq('Folder0/Folder1/Folder3');
+	});
+});
+
+describe("When replacing the token in files", function(){
+	it("Then the file data should not contain the token", function(done) {
+
+		var testData = "Template Module";
+		var subjectUnderTest = require('../src/fs-manager')(fakeFs, '');
+		var file = { name: "TestFile", path: "." };
+
+
+		subjectUnderTest.replaceStringInTemplateFile(file, "Template", "Replaced", function() {
+			done();
+		});
 	});
 });
